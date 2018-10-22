51b3e101c2f9e56c2639bb0b5f583c5ca7f9dd64
Tantely Randria <tantely.tnr@gmail.com>
2018-10-23 00:24:59 +0200

    création fichier gitignore

---
+++ another_file.js
@@ -0,0 +1,2 @@
+I'm very fine thanks, and you?
+For today we have to learn Bootstrap and after that we must check how to do a report.
---
+++ same_file.js
@@ -0,0 +1,3 @@
+Hello team Java Chronos
+How are you today
+I hope that you're fine.
